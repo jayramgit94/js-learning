5ccfb988cff76f5312843fb5f34dd6d63609fd77
Jayram sangawat <155347360+jayramgit94@users.noreply.github.com>
2024-10-09 06:30:53 +0000

    data types and intro

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,33 @@
+const accountId = 144553
+let accountEmail = "jayramsang@gmail.com" //all time use this 
+var accountPassword = "12345" // don't use this 
+accountCity = "Nagpur" // without writing let hum varibale define kr sakte he
+
+
+let accountstate;//eski value undefing hogi kyunki eski value define nhi  ki he 
+
+
+
+// accountId = 2 // not allowed bec it is constant 
+/*
+this is multiple comment comment section presfer use to write a notes on it 
+
+-- prefer not to use var 
+bec of issue in block scope and functional scope
+
+
+{
+inko scpoe kahte he 
+
+}
+
+*/
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "121"
+accountCity = "ngp"
+
+console.log(accountId);
+
+console.table([accountId,accountPassword,accountCity, accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version 
+
+//alert(2+2) // we are using nodejs, not browsers [shwoinng error]
+
+//code readability should be high 
+console.log(3+3);
+console.log("jayram");
+
+let name = "jayram"
+let age = 18
+let islooged = true
+let state;
+
+//number 2 to the power 53
+//bigint
+//strig => ""
+//boolean => true or flase
+// null => standalone value representaion of empty value
+// unrefined => 
+//symbol => uniqueness
+
+// object 
+
+
+console.log(typeof "jayram");
+console.log(typeof age);
+
+// null is type of object   and undefind ka type undefined 
+
