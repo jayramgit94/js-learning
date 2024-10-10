ce6d7fa16ec5992ee7f635592451a1ce900d3720
Jayram sangawat <155347360+jayramgit94@users.noreply.github.com>
2024-10-10 18:20:16 +0000

    conversion and assignment operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,15 +2,59 @@
 // let score = "33abc" // number show convert karenge to kconvert ho jayega in number 
 // let score = null // show 0 
 // let score = undefined // show NaN
-let score = "jay" // show NaN
+// let score = "jay" // show NaN
 
-console.log(typeof score);
-console.log(typeof (score));
-
-
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof score);
+// console.log(typeof (score));
 
 
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
+
+
+/*********************operations******************************* */
+
+// let value = 3
+//  let negVlaue = -vaule
+//  console.log(negVlaue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// let str1 = "hello"
+// let str2 = " jayram"
+// let str3 = str1+str2
+// console.log(str3);
+
+
+// console.log("1"+ 2);
+// console.log("1"+ "2");
+// console.log(1+ "2");
+// console.log("1"+ 2+2);
+// console.log(1+2+ "2");
+// console.log(3+4*5%3);
+// console.log((3+4)*5%3);   //aese krna nhi he 
+
+
+console.log(+true);  //1
+// console.log(true+);  //error
+console.log(+""); // 0
+
+let num1 , num2 ,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+ gameCounter++;
+ console.log(gameCounter);
+
+ //postfix me increament  usage ke baad inc hoti he 
+ //pretfix me increament pahle increament hoti he 
+ 
