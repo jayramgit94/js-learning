73808d8074e3e8c4c5c4978bbfd41c5c107c6739
Jayram sangawat <155347360+jayramgit94@users.noreply.github.com>
2024-10-11 17:23:48 +0000

    comparison in js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,33 @@
+// console.log("2">1);//true
+// console.log("02">1); //true
+
+console.log(null > 0);//false
+console.log(null == 0);//false (bec esne convert kr diya he null ko 0 me so next wala true ho gaya he)
+console.log(null >= 0);//true
+
+//all are false 
+// in js comparison and equality checking is diff part
+console.log(  "  ");
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(  "  ");
+
+
+// === strictly check karta he 
+console.log("2"==2);//ye same he to true 
+console.log("2"===2);//ye same nhi he to false bec it checks the data type  
+
+
+/* -------------------datatypes summary---------------- */
+
+/**
+ * primitive-- 7 types 
+ * string , number, boolean, null, undefined, symbl, bigint
+ * 
+ * reference (non primitive)
+ * array , objects, functions 
+ * 
+ */
+
